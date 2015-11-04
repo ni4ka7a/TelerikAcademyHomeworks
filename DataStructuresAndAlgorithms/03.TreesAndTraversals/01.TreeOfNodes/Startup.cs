9782e1b86121fd01144d81f679c99ac32a0ed3e7
ni4ka7a <n.atanasow@yahoo.com>
2015-11-04 19:11:14 +0200

    Started homework TreesAndTraversals.

---
+++ DataStructuresAndAlgorithms/03.TreesAndTraversals/01.TreeOfNodes/Startup.cs
@@ -0,0 +1,73 @@
+﻿namespace _01.TreeOfNodes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var tree = ReadInput();
+
+            var root = FindRoot(tree);
+            Console.WriteLine(root.Value);
+
+            PrintTree(root);
+        }
+
+        private static void PrintTree(TreeNode<int> tree)
+        {
+            foreach (var item in tree.Children)
+            {
+                Console.WriteLine(item.Value);
+
+                PrintTree(item);
+            }
+        }
+
+        private static TreeNode<int> FindRoot(List<TreeNode<int>> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                if (node.HasParent == false)
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
+
+        private static List<TreeNode<int>> ReadInput()
+        {
+            var nodesCount = int.Parse(Console.ReadLine());
+            var nodes = new List<TreeNode<int>>();
+
+            for (int i = 0; i < nodesCount - 1; i++)
+            {
+                var line = Console.ReadLine().Split(' ').ToArray();
+                var parantValue = int.Parse(line[0]);
+                var childValue = int.Parse(line[1]);
+
+                var parent = nodes.FirstOrDefault(node => parantValue == node.Value);
+                if (parent == null)
+                {
+                    parent = new TreeNode<int>(parantValue);
+                    nodes.Add(parent);
+                }
+
+                var child = nodes.FirstOrDefault(node => childValue == node.Value);
+                if (child == null)
+                {
+                    child = new TreeNode<int>(childValue);
+                    nodes.Add(child);
+                }
+
+                parent.AddChild(child);
+            }
+
+            return nodes;
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/03.TreesAndTraversals/01.TreeOfNodes/TreeNode.cs
@@ -0,0 +1,66 @@
+﻿namespace _01.TreeOfNodes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class TreeNode<T>
+    {
+        private T value;
+        private List<TreeNode<T>> children;
+
+        public TreeNode()
+        {
+            this.children = new List<TreeNode<T>>();
+        }
+
+        public TreeNode(T value)
+            : this()
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("Cannot insert null value!");
+            }
+
+            this.Value = value;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return this.value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public bool HasParent { get; set; }
+
+        public List<TreeNode<T>> Children
+        {
+            get
+            {
+                return this.children;
+            }
+        }
+
+        public void AddChild(TreeNode<T> child)
+        {
+            if (child == null)
+            {
+                throw new ArgumentNullException("Cannot insert null value!");
+            }
+
+            if (child.HasParent)
+            {
+                throw new ArgumentException("The node already has a parent!");
+            }
+
+            child.HasParent = true;
+            this.children.Add(child);
+        }
+    }
+}
