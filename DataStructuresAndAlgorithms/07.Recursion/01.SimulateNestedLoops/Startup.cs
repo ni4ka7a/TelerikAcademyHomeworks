e8fd36e945435cd672daae55dd0b1093dfeae832
Nikolay Atanasow <Nikolay Atanasow>
2015-11-06 14:18:17 +0200

    Task 1 SimulateNestedLoops for Homework Recursion added.

---
+++ DataStructuresAndAlgorithms/07.Recursion/01.SimulateNestedLoops/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.SimulateNestedLoops
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var n = 2;
+            SimulateLoop(1, 1, n);
+        }
+
+        private static void SimulateLoop(int i, int j, int n)
+        {
+            Console.WriteLine("{0} {1}", i, j);
+
+            if (j >= n)
+            {
+                return;
+            }
+
+            SimulateLoop(i, j + 1, n);
+
+            if (i >= n)
+            {
+                return;
+            }
+
+            SimulateLoop(i + 1, j, n);
+        }
+    }
+}
