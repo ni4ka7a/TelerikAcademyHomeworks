c7eed538a6a91d8c55ce75639841ac7b3e601b87
Nikolay Atanasow <Nikolay Atanasow>
2015-11-06 16:40:34 +0200

    Added project for task 2 GenerateCombinationsWithDuplicates for Recursion homework.

---
+++ DataStructuresAndAlgorithms/07.Recursion/02.GenerateCombinationsWithDuplicates/Startup.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.GenerateCombinationsWithDuplicates
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var n = 3;
+            GenerateCombinations(n, 2);
+        }
+
+        private static void GenerateCombinations(int n, int k)
+        {
+        }
+    }
+}
