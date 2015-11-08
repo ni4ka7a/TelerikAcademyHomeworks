c277b8ebff3b904ca940da81a1f58d8996316d60
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-08 21:45:41 +0200

    Task 3 - Generate Combinations without duplicates for Recursion homework added.

---
+++ DataStructuresAndAlgorithms/07.Recursion/03.GenerateCombinationsWithoutDuplications/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace _03.GenerateCombinationsWithoutDuplications
+{
+    using System;
+
+    public class Startup
+    {
+        private static int[] arr;
+
+        public static void Main(string[] args)
+        {
+            var n = 4; // Console.ReadLine();
+            var k = 2; // Console.ReadLine();
+
+            arr = new int[k];
+            GenerateCombinations(0, 1, n);
+        }
+
+        private static void GenerateCombinations(int index, int start, int end)
+        {
+            if (index == arr.Length)
+            {
+                Console.WriteLine("({0})", string.Join(", ", arr));
+                return;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                arr[index] = i;
+                GenerateCombinations(index + 1, i + 1, end);
+            }
+        }
+    }
+}
