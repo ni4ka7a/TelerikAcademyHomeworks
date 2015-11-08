ea79825649638e57f94f8c1b92812ff90217ef0c
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-08 23:10:57 +0200

    Task 6 - Generate variations without repetitions for homework recursion added.

---
+++ DataStructuresAndAlgorithms/07.Recursion/06.GenerateVariationsWithoutRepetitions/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace _06.GenerateVariationsWithoutRepetitions
+{
+    using System;
+
+    public class Startup
+    {
+        private static string[] inputValues = new string[] { "test", "rock", "fun" };
+
+        public static void Main(string[] args)
+        {
+            var k = 2; // Console.ReadLine();
+            int[] arr = new int[k];
+
+            GenerateVariationsNoRepetitions(0, k, arr);
+        }
+
+        private static void GenerateVariationsNoRepetitions(int index, int k, int[] arr)
+        {
+            if (k == 0)
+            {
+                Print(arr);
+            }
+            else
+            {
+                for (int i = index; i < inputValues.Length; i++)
+                {
+                    arr[k - 1] = i;
+                    GenerateVariationsNoRepetitions(i + 1, k - 1, arr);
+                }
+            }
+        }
+
+        private static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(inputValues[arr[i]] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
