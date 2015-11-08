de1846632c05ea0174927e9d006ba56033ff7261
Nikolay Atanasow <Nikolay Atanasow>
2015-11-08 21:35:18 +0200

    Task 2 - Generate Combinations with Duplicates for homework Recursion added.

--- DataStructuresAndAlgorithms/07.Recursion/02.GenerateCombinationsWithDuplicates/Startup.cs
+++ DataStructuresAndAlgorithms/07.Recursion/02.GenerateCombinationsWithDuplicates/Startup.cs
@@ -4,14 +4,30 @@
 
     public class Startup
     {
+        private static int[] arr;
+
         public static void Main(string[] args)
         {
-            var n = 3;
-            GenerateCombinations(n, 2);
+            var n = 3; // Console.ReadLine();
+            var k = 2; // Console.ReadLine();
+
+            arr = new int[k];
+            GenerateCombinations(0, 1, n);
         }
 
-        private static void GenerateCombinations(int n, int k)
+        private static void GenerateCombinations(int index, int start, int end)
         {
+            if (index == arr.Length)
+            {
+                Console.WriteLine("({0})", string.Join(", ", arr));
+                return;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                arr[index] = i;
+                GenerateCombinations(index + 1, start, end);
+            }
         }
     }
 }
