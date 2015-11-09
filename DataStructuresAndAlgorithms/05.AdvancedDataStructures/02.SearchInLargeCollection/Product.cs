5c1783718cda6fad0b2fd03a3adc66abcee7110f
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-09 23:56:51 +0200

    Added solution for homework Advanced Data Structures and taks 2 - SearchInLargeCollection implemented.

---
+++ DataStructuresAndAlgorithms/05.AdvancedDataStructures/02.SearchInLargeCollection/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.SearchInLargeCollection
+{
+    using System;
+
+    public class Product : IComparable
+    {
+        public Product(string name, int price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var productToCompare = obj as Product;
+
+            return this.Price - productToCompare.Price;
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/05.AdvancedDataStructures/02.SearchInLargeCollection/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.SearchInLargeCollection
+{
+    using System;
+    using System.Linq;
+
+    using Wintellect.PowerCollections;
+
+    public class Startup
+    {
+        private static readonly Random Random = new Random();
+
+        public static void Main(string[] args)
+        {
+            var products = AddProducts(500000);
+
+            var startProduct = new Product("Pesho", 100);
+            var endProduct = new Product("Pesho", 200);
+
+            var filteredProducts = products.Range(startProduct, true, endProduct, true).Take(20);
+
+            foreach (var product in filteredProducts)
+            {
+                Console.WriteLine("Name: {0}    Price:{1}", product.Name, product.Price);
+            }
+        }
+
+        private static OrderedBag<Product> AddProducts(int numberOfProducts)
+        {
+            var products = new OrderedBag<Product>();
+
+            for (int i = 1; i < numberOfProducts; i++)
+            {
+                products.Add(new Product(
+                    "Name " + i,
+                    Random.Next(1, 1000)));
+            }
+
+            return products;
+        }
+    }
+}
