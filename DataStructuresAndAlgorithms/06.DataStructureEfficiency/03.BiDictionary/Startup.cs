da744d6d03149d65688285a692264ec65f001260
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-10 12:01:21 +0200

    Task 3 - BiDictionray for homework Data Structure Efficiency added.

---
+++ DataStructuresAndAlgorithms/06.DataStructureEfficiency/03.BiDictionary/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace _03.BiDictionary
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var dict = new BiDictionary<int, int, string>();
+
+            dict.Add(1, 1, "Pesho");
+            dict.Add(1, 21, "Gosho");
+            dict.Add(2, 1, "Stamat");
+            dict.Add(1, 3, "Mariika");
+
+            Console.WriteLine(dict[1, 3]);
+            Console.WriteLine(dict[1, 1]);
+
+            var collectionByKey2 = dict.GetByKey2(1);
+            Console.WriteLine(string.Join(", ", collectionByKey2));  
+        }
+    }
+}
