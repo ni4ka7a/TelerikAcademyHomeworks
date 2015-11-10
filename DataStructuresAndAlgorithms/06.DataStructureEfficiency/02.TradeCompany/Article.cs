164d5f8053064fc1f202dcd15ad2e7d67cdd88d1
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-10 11:47:59 +0200

    Task 2 - TradeCompany for homework Data Structure Efficiency added.

---
+++ DataStructuresAndAlgorithms/06.DataStructureEfficiency/02.TradeCompany/Article.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.TradeCompany
+{
+    using System;
+
+    public class Article : IComparable<Article>
+    {
+        public Article(string barcode, string vendor, string title, decimal price)
+        {
+            this.Barcode = barcode;
+            this.Vendor = vendor;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        public string Barcode { get; set; }
+
+        public string Vendor { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CompareTo(Article other)
+        {
+            return (int)(this.Price - other.Price);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/06.DataStructureEfficiency/02.TradeCompany/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.TradeCompany
+{
+    using System;
+    using System.Linq;
+
+    using Wintellect.PowerCollections;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var articles = GenerateArticles(50000);
+
+            var articlesInRange = articles.Range(4m, true, 50m, true);
+
+            foreach (var item in articlesInRange)
+            {
+                Console.WriteLine("{0} - {1}", item.Key, item.Value.FirstOrDefault().Title);
+            }
+        }
+
+        private static OrderedMultiDictionary<decimal, Article> GenerateArticles(int numberOfArticles)
+        {
+            var articles = new OrderedMultiDictionary<decimal, Article>(true);
+
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                var article = new Article(
+                    i + string.Empty + i + 1 + string.Empty + i + 2,
+                    "Vendor " + i,
+                    "Title " + i,
+                    (decimal)(i + Math.Sqrt(i)));
+
+                articles.Add(article.Price, article);
+            }
+
+            return articles;
+        }
+    }
+}
