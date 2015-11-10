9bf328723546393367e32aae2f53e9f1db466ad8
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-10 11:32:38 +0200

    Task 1 - OrderStudents for homework Data Structure Efficiency added.

--- DataStructuresAndAlgorithms/06.DataStructureEfficiency/01.OrderStudents/Startup.cs
+++ DataStructuresAndAlgorithms/06.DataStructureEfficiency/01.OrderStudents/Startup.cs
@@ -1,12 +1,43 @@
 ﻿namespace _01.OrderStudents
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
 
     public class Startup
     {
         public static void Main(string[] args)
         {
+            var reader = new StreamReader("../../students.txt");
 
+            var courses = ReadStudentsFromText(reader);
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine("{0}: {1}", course.Key, string.Join(", ", course.Value));
+            }
+        }
+
+        private static SortedDictionary<string, SortedSet<string>> ReadStudentsFromText(StreamReader reader)
+        {
+            var students = new SortedDictionary<string, SortedSet<string>>();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine().Split('|');
+                var studentFullName = line[0].Trim() + " " + line[1].Trim();
+                var courseName = line[2].Trim();
+
+                if (!students.ContainsKey(courseName))
+                {
+                    students[courseName] = new SortedSet<string>();
+                }
+
+                students[courseName].Add(studentFullName);
+            }
+
+            return students;
         }
     }
 }
