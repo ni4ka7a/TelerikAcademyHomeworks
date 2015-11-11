b4b9084cbcae224098b1f5616e7870ed72cf6c19
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-11 16:41:06 +0200

    Task 2 - Rabbits for homework Combinatorics added.

---
+++ DataStructuresAndAlgorithms/08.Combinatorics/02.Rabbits/Startup.cs
@@ -0,0 +1,49 @@
+﻿namespace _02.Rabbits
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var inputRabitts = int.Parse(Console.ReadLine());
+            var uniqueRabbits = new Dictionary<ulong, ulong>();
+
+            for (int i = 0; i < inputRabitts; i++)
+            {
+                ulong currentNumber = ulong.Parse(Console.ReadLine());
+
+                if (!uniqueRabbits.ContainsKey(currentNumber))
+                {
+                    uniqueRabbits[currentNumber] = 0;
+                }
+
+                uniqueRabbits[currentNumber]++;
+            }
+
+            ulong sum = 0;
+
+            foreach (var item in uniqueRabbits)
+            {
+                if (item.Value > item.Key + 1)
+                {
+                    var magicNumber = item.Value / (item.Key + 1);
+
+                    if (item.Value % (item.Key + 1) != 0)
+                    {
+                        magicNumber += 1;
+                    }
+
+                    sum += magicNumber * (item.Key + 1);
+                }
+                else
+                {
+                    sum += item.Key + 1;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
