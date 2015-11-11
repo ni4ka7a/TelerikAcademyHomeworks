0386657570220c162cde3800e0e6498bf51e2756
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-11 16:37:57 +0200

    Task 1 - Passwords for homework Combinatorics added.

---
+++ DataStructuresAndAlgorithms/08.Combinatorics/01.Passwords/Startup.cs
@@ -0,0 +1,14 @@
+﻿namespace _01.Passwords
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var starsCounter = Console.ReadLine().ToCharArray().Where(x => x == '*').Count();
+            Console.WriteLine((ulong)Math.Pow(2, starsCounter));
+        }
+    }
+}
