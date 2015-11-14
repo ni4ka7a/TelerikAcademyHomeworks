04e0eb5029ef95a16cd111e46e98370878d893bf
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-14 23:41:39 +0200

    Solution for WCF homework and simple service added.

---
+++ WebServicesAndCloud/06.WindowsCommunicationFoundation/DayOfWeek/DayService.svc.cs
@@ -0,0 +1,13 @@
+﻿namespace DayOfWeek
+{
+    using System;
+
+    public class DayService : IDayService
+    {
+        public string GetDayOfWeek(DateTime date)
+        {
+            var culture = new System.Globalization.CultureInfo("bg-BG");
+            return culture.DateTimeFormat.GetDayName(DateTime.Today.DayOfWeek);
+        }
+    }
+}
---
+++ WebServicesAndCloud/06.WindowsCommunicationFoundation/DayOfWeek/IDayService.cs
@@ -0,0 +1,12 @@
+﻿namespace DayOfWeek
+{
+    using System;
+    using System.ServiceModel;
+
+    [ServiceContract]
+    public interface IDayService
+    {
+        [OperationContract]
+        string GetDayOfWeek(DateTime date);
+    }
+}
