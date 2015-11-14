426b220f9a31b083b3381e6e27ca09f3aff13dee
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-15 00:18:05 +0200

    ConsoleClient for StringsService for WCF homework added.

---
+++ WebServicesAndCloud/06.WindowsCommunicationFoundation/StringsOperations.ConsoleClient/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace StringsOperations.ConsoleClient
+{
+    using System;
+
+    using StringsService;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            StringsServiceClient client = new StringsServiceClient();
+
+            var firstString = "hello";
+            var secondString = "hello pesho, hello";
+
+            var result = client.NumberOfContains(firstString, secondString);
+            Console.WriteLine(result);
+        }
+    }
+}
