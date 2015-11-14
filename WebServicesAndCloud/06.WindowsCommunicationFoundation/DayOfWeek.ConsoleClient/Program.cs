2d3eb8c9cee1240ecb9fb2b0d7a427f225c1d902
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-14 23:48:46 +0200

    Simple console client for DayService for WCF homework added.

---
+++ WebServicesAndCloud/06.WindowsCommunicationFoundation/DayOfWeek.ConsoleClient/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DayOfWeek.ConsoleClient
+{
+    using System;
+
+    using DayOfWeekService;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DayServiceClient client = new DayServiceClient();
+
+            var result = client.GetDayOfWeek(DateTime.Now);
+            Console.WriteLine(result);
+        }
+    }
+}
