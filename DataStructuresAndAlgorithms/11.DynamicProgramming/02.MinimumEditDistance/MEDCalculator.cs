4205339f090fc0188a3afc7d4874086a4a622686
Nikolay Atanasow <n.atanasow@yahoo.com>
2015-11-25 23:13:42 +0200

    Task MinimumEditDistance added.

---
+++ DataStructuresAndAlgorithms/11.DynamicProgramming/02.MinimumEditDistance/MEDCalculator.cs
@@ -0,0 +1,98 @@
+﻿namespace _02.MinimumEditDistance
+{
+    using System;
+
+    public class MEDCalculator
+    {
+        private decimal deleteCost;
+        private decimal insertCost;
+        private decimal replaceCost;
+
+        public MEDCalculator(decimal insertCost, decimal deleteCost, decimal replaceCost)
+        {
+            this.insertCost = insertCost;
+            this.deleteCost = deleteCost;
+            this.replaceCost = replaceCost;
+        }
+
+        public decimal Calculate(string target, string input)
+        {
+            var distance_table = new double[input.Length + 1, target.Length + 1];
+
+            for (int k = 0; k <= input.Length; k++)
+            {
+                distance_table[k, 0] = k * 0.7;
+            }
+
+            for (int k = 0; k <= target.Length; k++)
+            {
+                distance_table[0, k] = k * 0.7;
+            }
+
+            for (int k = 1; k <= input.Length; k++)
+            {
+                for (int d = 1; d <= target.Length; d++)
+                {
+                    if (input[k - 1] == target[d - 1])
+                    {
+                        distance_table[k, d] = distance_table[k - 1, d - 1];
+                    }
+                    else
+                    {
+                        distance_table[k, d] = Math.Min(
+                            Math.Min(distance_table[k - 1, d - 1] + 1, distance_table[k - 1, d] + 0.7), distance_table[k, d - 1] + 0.7);
+                    }
+                }
+            }
+
+            decimal totalCost = 0;
+            int i = input.Length, j = target.Length;
+
+            while (true)
+            {
+                if (i == 0 && j == 0)
+                {
+                    return totalCost;
+                }
+                else if (i == 0 && j > 0)
+                {
+                    totalCost += this.deleteCost;
+                    j--;
+                }
+                else if (i > 0 && j == 0)
+                {
+                    totalCost += this.insertCost;
+                    i--;
+                }
+                else
+                {
+                    if (distance_table[i - 1, j - 1] <= distance_table[i - 1, j] &&
+                            distance_table[i - 1, j - 1] <= distance_table[i, j - 1])
+                    {
+                        if (distance_table[i - 1, j - 1] == distance_table[i, j])
+                        {
+                            totalCost += 0;
+                        }
+                        else
+                        {
+                            totalCost += this.replaceCost;
+                        }
+
+                        i--;
+                        j--;
+                    }
+                    else if (distance_table[i - 1, j] < distance_table[i, j - 1])
+                    {
+                        totalCost += this.insertCost;
+                        i--;
+                    }
+                    else if (distance_table[i, j - 1] < distance_table[i - 1, j])
+                    {
+                        totalCost += this.deleteCost;
+                        j--;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/11.DynamicProgramming/02.MinimumEditDistance/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.MinimumEditDistance
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var medCalculator = new MEDCalculator(1, 0.9m, 0.8m);
+
+            var input = new List<string[]>();
+            input.Add(new string[] { "developer", "enveloped" });
+            input.Add(new string[] { "developer", "eveloper" });
+            input.Add(new string[] { "eveloper", "enveloper" });
+            input.Add(new string[] { "r", "d" });
+
+            foreach (var pair in input)
+            {
+                Console.WriteLine("{0, -10} | {1, -10} | {2}", pair[0], pair[1], medCalculator.Calculate(pair[0], pair[1]));
+            }
+        }
+    }
+}
