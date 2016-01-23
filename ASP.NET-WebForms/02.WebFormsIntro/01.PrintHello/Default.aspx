2648a3c7b6175fd906b5553c98c0523242ad35cf
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-01-23 18:11:58 +0200

    Task 1.PrintHello for homework WebForms intro added.

---
+++ ASP.NET-WebForms/02.WebFormsIntro/01.PrintHello/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_01.PrintHello.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="tbName" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSumbit" runat="server" Text="submit" OnClick="submit_Click" />
+
+            <div>
+                <asp:Label ID="output" runat="server"></asp:Label>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-WebForms/02.WebFormsIntro/01.PrintHello/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _01.PrintHello
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            var name = this.tbName.Text;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                this.output.Text = "Invalid name!";
+            }
+
+            else
+            {
+                this.output.Text = "Hello " + name;
+            }
+        }
+    }
+}
