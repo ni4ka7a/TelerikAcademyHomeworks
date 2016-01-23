96bff90afe4226f8574a30e510942bedf17088c4
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-01-24 00:03:13 +0200

    Task 03.StudentsAndCourses for homework WebControlsAndHtmlControls added.

---
+++ ASP.NET-WebForms/03.WebControlsAndHTMLControls/03.StudentsAndCourses/Registration.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="_03.StudentsAndCourses.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label Text="First Name:" runat="server" />
+            <asp:TextBox ID="tb_firstName" runat="server" />
+        </div>
+        <div>
+            <asp:Label Text="Last Name:" runat="server" />
+            <asp:TextBox ID="tb_lastName" runat="server" />
+        </div>
+        <div>
+            <asp:Label Text="Faculty Number:" runat="server" />
+            <asp:TextBox ID="tb_facultyNumber" runat="server" />
+        </div>
+        <div>
+            <asp:Label Text="University:" runat="server" />
+            <asp:DropDownList ID="ddl_university" runat="server">
+                <asp:ListItem Text="TU-Sofia" />
+                <asp:ListItem Text="Sofia University" />
+                <asp:ListItem Text="NBU" />
+            </asp:DropDownList>
+        </div>
+        <div>
+            <asp:Label Text="Specialty:" runat="server" />
+            <asp:DropDownList ID="ddl_specialty" runat="server">
+                <asp:ListItem Text="Telecommunications" />
+                <asp:ListItem Text="Computer Science" />
+                <asp:ListItem Text="Elektrotechnik" />
+                <asp:ListItem Text="Marketing" />
+            </asp:DropDownList>
+        </div>
+        <div>
+            <asp:Label Text="Courses:" runat="server" />
+            <br />
+            <asp:ListBox ID="lb_courses" SelectionMode="Multiple" runat="server">
+                <asp:ListItem Text="Neural Network" />
+                <asp:ListItem Text="Marketing Fundamentals" />
+                <asp:ListItem Text="Radiocommunications" />
+                <asp:ListItem Text="Elektrotechnik part 1" />
+            </asp:ListBox>
+        </div>
+        <div>
+            <asp:Button Text="Submit" ID="btn_submit" OnClick="Submit_Click" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-WebForms/03.WebControlsAndHTMLControls/03.StudentsAndCourses/Registration.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace _03.StudentsAndCourses
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        private const string Error_Message = "cannot be null or empty!";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            var firstName = this.tb_firstName.Text;
+            var lastName = this.tb_lastName.Text;
+            var facultyNumber = this.tb_facultyNumber.Text;
+            var university = this.ddl_university.Text;
+            var specialty = this.ddl_specialty.Text;
+            var courses = this.lb_courses.Items;
+
+            if (string.IsNullOrEmpty(firstName))
+            {
+                var errorLabel = new Label();
+                errorLabel.Text = "First name " + Error_Message;
+                this.Controls.Add(errorLabel);
+            }
+
+            else if (string.IsNullOrEmpty(lastName))
+            {
+                var errorLabel = new Label();
+                errorLabel.Text = "Last name " + Error_Message;
+                this.Controls.Add(errorLabel);
+            }
+
+            else if (string.IsNullOrEmpty(facultyNumber))
+            {
+                var errorLabel = new Label();
+                errorLabel.Text = "Faculty number " + Error_Message;
+                this.Controls.Add(errorLabel);
+            }
+
+            else
+            {
+                var br = new HtmlGenericControl("br");
+
+                var heading = new HtmlGenericControl("h1");
+                heading.InnerText = "Student information";
+
+                var firstNameContainer = new HtmlGenericControl("div");
+                var firstNameLabel = new HtmlGenericControl("strong");
+                var firstNameValue = new HtmlGenericControl("span");
+                firstNameLabel.InnerText = "First Name: ";
+                firstNameValue.InnerText = firstName;
+                firstNameContainer.Controls.Add(firstNameLabel);
+                firstNameContainer.Controls.Add(firstNameValue);
+
+                var lastNameContainer = new HtmlGenericControl("div");
+                var lastNameLabel = new HtmlGenericControl("strong");
+                var lastNameValue = new HtmlGenericControl("span");
+                lastNameLabel.InnerText = "Last Name: ";
+                lastNameValue.InnerText = lastName;
+                lastNameContainer.Controls.Add(lastNameLabel);
+                lastNameContainer.Controls.Add(lastNameValue);
+
+
+                var facultyNumberContainer = new HtmlGenericControl("div");
+                var facultyNumberLabel = new HtmlGenericControl("strong");
+                var facultyNumberValue = new HtmlGenericControl("span");
+                facultyNumberLabel.InnerText = "Faculty Number:";
+                facultyNumberValue.InnerText = facultyNumber;
+                facultyNumberContainer.Controls.Add(facultyNumberLabel);
+                facultyNumberContainer.Controls.Add(facultyNumberValue);
+
+
+                var universityContainer = new HtmlGenericControl("div");
+                var universityLabel = new HtmlGenericControl("strong");
+                var universityValue = new HtmlGenericControl("span");
+                universityLabel.InnerText = "University: ";
+                universityValue.InnerText = university;
+                universityContainer.Controls.Add(universityLabel);
+                universityContainer.Controls.Add(universityValue);
+
+
+                var specialtyContainer = new HtmlGenericControl("div");
+                var specialtyLabal = new HtmlGenericControl("strong");
+                var specialtyValue = new HtmlGenericControl("sapn");
+                specialtyLabal.InnerText = "Specialty: ";
+                specialtyValue.InnerText = specialty;
+                specialtyContainer.Controls.Add(specialtyLabal);
+                specialtyContainer.Controls.Add(specialtyValue);
+
+                var coursesContainer = new HtmlGenericControl("div");
+                var coursesLabel = new HtmlGenericControl("strong");
+                var coursesValue = new HtmlGenericControl("ul");
+                coursesLabel.InnerText = "Courses: ";
+
+                foreach (ListItem item in courses)
+                {
+                    if (item.Selected)
+                    {
+                        var li = new HtmlGenericControl("li");
+                        li.InnerText = item.Text;
+                        coursesValue.Controls.Add(li);
+                    }
+                }
+
+                coursesContainer.Controls.Add(coursesLabel);
+                coursesContainer.Controls.Add(coursesValue);
+
+                this.Controls.Add(br);
+                this.Controls.Add(br);
+                this.Controls.Add(heading);
+                this.Controls.Add(firstNameContainer);
+                this.Controls.Add(lastNameContainer);
+                this.Controls.Add(facultyNumberContainer);
+                this.Controls.Add(specialtyContainer);
+                this.Controls.Add(coursesContainer);
+            }
+
+        }
+    }
+}
