6c07b25d0f486fecf6c6089d00c761c667da0490
Nikolay Atanasow <n.atanasow@yahoo.com>
2016-01-24 12:06:16 +0200

    Task 04.TicTacToe for homework WebControlsAndHtmlCOntrols added.

---
+++ ASP.NET-WebForms/03.WebControlsAndHTMLControls/04.TicTacToe/TicTacToe.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TicTacToe.aspx.cs" Inherits="_04.TicTacToe.TicTacToe" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="~/Content/Site.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="bform1" runat="server">
+        <asp:Table ID="field" runat="server">
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b1" CommandArgument="b1" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b2" CommandArgument="b2" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b3" CommandArgument="b3" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b4" CommandArgument="b4" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b5" CommandArgument="b5" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b6" CommandArgument="b6" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b7" CommandArgument="b7" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b8" CommandArgument="b8" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:ImageButton ImageUrl="~/Content/blank.png" ID="b9" CommandArgument="b9" OnCommand="Play" runat="server" />
+                </asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <div id="startOver">
+            <asp:Button Text="Start Over" OnClick="StartOver" runat="server" />
+        </div>
+        <div>
+            <asp:Label ID="output" Text="" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-WebForms/03.WebControlsAndHTMLControls/04.TicTacToe/TicTacToe.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _04.TicTacToe
+{
+    public partial class TicTacToe : System.Web.UI.Page
+    {
+        private const string BlankImageUrl = "~/Content/blank.png";
+        private const string XImageUrl = "~/Content/x.png";
+        private const string OImageUrl = "~/Content/o.png";
+        private List<ImageButton> buttons;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            buttons = new List<ImageButton>();
+
+            foreach (TableRow row in this.field.Controls)
+            {
+                foreach (TableCell item in row.Controls)
+                {
+                    var imgButton = item.Controls[0] as ImageButton;
+
+                    if (imgButton != null)
+                    {
+                        buttons.Add(imgButton);
+                    }
+                }
+            }
+        }
+
+        protected void Play(object sender, CommandEventArgs e)
+        {
+
+            var currentButton = this.buttons.FirstOrDefault(b => b.ID == e.CommandArgument.ToString());
+
+            if (currentButton.ImageUrl == BlankImageUrl)
+            {
+                currentButton.ImageUrl = XImageUrl;
+
+                var aiMove = this.buttons
+                    .Where(b => b.ImageUrl == BlankImageUrl)
+                    .OrderBy(b => Guid.NewGuid())
+                    .FirstOrDefault();
+
+                if (aiMove != null)
+                {
+                    aiMove.ImageUrl = OImageUrl;
+                }
+            }
+
+            var winner = this.CheckDiagonals();
+            if (winner == XImageUrl)
+            {
+                this.output.Text = "You WON!!!";
+                this.DisableButtensOnGameOver();
+            }
+            else if(winner == OImageUrl)
+            {
+                this.output.Text = "You Loose!!!";
+                this.DisableButtensOnGameOver();
+            }
+
+            winner = this.CheckRows();
+
+            if (winner == XImageUrl)
+            {
+                this.output.Text = "You WON!!!";
+                this.DisableButtensOnGameOver();
+            }
+            else if (winner == OImageUrl)
+            {
+                this.output.Text = "You Loose!!!";
+                this.DisableButtensOnGameOver();
+            }
+
+            winner = this.CheckCols();
+
+            if (winner == XImageUrl)
+            {
+                this.output.Text = "You WON!!!";
+                this.DisableButtensOnGameOver();
+            }
+            else if (winner == OImageUrl)
+            {
+                this.output.Text = "You Loose!!!";
+                this.DisableButtensOnGameOver();
+            }
+        }
+
+        protected void StartOver(object sender, EventArgs e)
+        {
+            Response.Redirect("~/TicTacToe.aspx");
+        }
+
+        private void DisableButtensOnGameOver()
+        {
+            foreach (var item in this.buttons)
+            {
+                item.OnClientClick = "this.disabled = true;";
+            }
+        }
+
+        private string CheckDiagonals()
+        {
+            if (this.buttons[0].ImageUrl == this.buttons[4].ImageUrl &&
+                this.buttons[0].ImageUrl == this.buttons[8].ImageUrl)
+            {
+                return this.buttons[0].ImageUrl;
+            }
+
+            else if(this.buttons[2].ImageUrl == this.buttons[4].ImageUrl &&
+                    this.buttons[2].ImageUrl == this.buttons[6].ImageUrl)
+            {
+                return this.buttons[2].ImageUrl;
+            }
+
+            return null;
+        }
+
+        private string CheckCols()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (this.buttons[i].ImageUrl == this.buttons[i + 3].ImageUrl &&
+                    this.buttons[i].ImageUrl == this.buttons[i + 6].ImageUrl)
+                {
+                    return this.buttons[i].ImageUrl;
+                }
+            }
+
+            return null;
+        }
+
+        private string CheckRows()
+        {
+            for (int i = 0; i < this.buttons.Count; i += 3)
+            {
+                if (this.buttons[i].ImageUrl == this.buttons[i + 1].ImageUrl &&
+                    this.buttons[i].ImageUrl == this.buttons[i + 2].ImageUrl)
+                {
+                    return this.buttons[i].ImageUrl;
+                }
+            }
+
+            return null;
+        }
+    }
+}
